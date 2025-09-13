1992abcc04d7b7c9fcfb80cde2e5ffe1c6c9eba8
surryaansh <suryanshs1804@gmail.com>
2025-09-13 21:48:07 +0530

    Update DarkModeToggle.tsx

--- components/DarkModeToggle.tsx
+++ components/DarkModeToggle.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import { SunIcon } from './icons/SunIcon';
-import { MoonIcon } from './icons/MoonIcon';
+import { SunIcon } from './icons/SunIcon.tsx';
+import { MoonIcon } from './icons/MoonIcon.tsx';
 
 interface DarkModeToggleProps {
   isDarkMode: boolean;
