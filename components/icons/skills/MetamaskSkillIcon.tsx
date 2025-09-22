434b3ae63e4cd1913ff447ae0a5081cb68618c85
surryaansh <suryanshs1804@gmail.com>
2025-09-22 17:55:22 +0530

    Add files via upload

---
+++ components/icons/skills/MetamaskSkillIcon.tsx
@@ -0,0 +1,61 @@
+
+import React from 'react';
+
+interface IconProps {
+  className?: string;
+}
+
+export const MetamaskSkillIcon: React.FC<IconProps> = ({ className }) => {
+  return (
+    <div className={`${className} flex items-center justify-center`}>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        version="1.1"
+        viewBox="0 0 2500 494"
+        className="w-full h-auto"
+      >
+        <g>
+          <path fill="currentColor" d="M2208.2,248.3c-13-8.6-27.3-14.7-40.9-22.3c-8.8-5-18.1-9.3-25.8-15.6c-13-10.7-10.3-31.6,3.3-40.7 c19.5-13,51.7-5.7,55.2,20.8c0,0.6,0.6,1,1.1,1h29.4c0.8,0,1.3-0.6,1.2-1.3c-1.5-18.3-8.6-33.5-21.6-43.2 c-12.4-9.3-26.5-14.3-41.6-14.3c-77.7,0-84.7,82.1-42.9,107.9c4.8,3,45.8,23.6,60.3,32.6c14.5,9,19.1,25.3,12.8,38.3 c-5.7,11.8-20.6,20-35.5,19c-16.2-1-28.8-9.7-33.2-23.4c-0.8-2.5-1.1-7.2-1.1-9.3c0-0.6-0.6-1.1-1.1-1.1h-31.9 c-0.6,0-1.1,0.6-1.1,1.1c0,23,5.7,35.8,21.4,47.4c14.7,11,30.7,15.6,47.3,15.6c43.5,0,66-24.6,70.6-50.1 C2238,285.6,2230.6,263.2,2208.2,248.3L2208.2,248.3z" />
+          <path fill="currentColor" d="M824.2,136.8H810h-15.5c-0.6,0-1,0.4-1.2,0.8l-26.1,86.1c-0.4,1.1-1.9,1.1-2.3,0l-26.2-86.1 c-0.2-0.6-0.6-0.8-1.1-0.8h-15.5h-14.1H689c-0.6,0-1.2,0.6-1.2,1.2v219.7c0,0.6,0.6,1.1,1.2,1.1h31.9c0.6,0,1.2-0.6,1.2-1.1v-167 c0-1.3,1.9-1.5,2.3-0.4l26.3,86.6l1.9,6.1c0.2,0.6,0.6,0.8,1.1,0.8h24.4c0.6,0,1-0.4,1.1-0.8l1.9-6.1l26.3-86.6 c0.4-1.3,2.3-1,2.3,0.4v167c0,0.6,0.6,1.1,1.1,1.1h31.9c0.6,0,1.1-0.6,1.1-1.1V137.9c0-0.6-0.6-1.2-1.1-1.2L824.2,136.8 L824.2,136.8z" />
+          <path fill="currentColor" d="M1720.2,136.8c-0.6,0-1,0.4-1.2,0.8l-26.1,86.1c-0.4,1.1-1.9,1.1-2.3,0l-26.2-86.1 c-0.2-0.6-0.6-0.8-1.2-0.8h-48.5c-0.6,0-1.2,0.6-1.2,1.2v219.7c0,0.6,0.6,1.1,1.2,1.1h31.9c0.6,0,1.2-0.6,1.2-1.1v-167 c0-1.3,1.9-1.5,2.3-0.4l26.3,86.6l1.9,6.1c0.2,0.6,0.6,0.8,1.2,0.8h24.4c0.6,0,0.9-0.4,1.1-0.8l1.9-6.1l26.4-86.6 c0.4-1.3,2.3-1,2.3,0.4v167c0,0.6,0.6,1.1,1.1,1.1h31.9c0.6,0,1.2-0.6,1.2-1.1V137.9c0-0.6-0.6-1.2-1.2-1.2L1720.2,136.8 L1720.2,136.8z" />
+          <path fill="currentColor" d="M1309,136.8h-59.4h-31.9h-59.4c-0.6,0-1.1,0.6-1.1,1.2v27.4c0,0.6,0.6,1.2,1.1,1.2h58.2v191.2 c0,0.6,0.6,1.1,1.1,1.1h31.9c0.6,0,1.1-0.6,1.1-1.1V166.5h58.2c0.6,0,1.1-0.6,1.1-1.2v-27.4C1310.1,137.3,1309.8,136.8,1309,136.8 L1309,136.8z" />
+          <path fill="currentColor" d="M1497,358.8h29c0.8,0,1.3-0.8,1.2-1.5l-59.9-220.5c-0.2-0.6-0.6-0.8-1.2-0.8H1455h-19.5h-11.1 c-0.6,0-1,0.4-1.2,0.8l-59.9,220.5c-0.2,0.8,0.4,1.5,1.2,1.5h29c0.6,0,1-0.4,1.2-0.8l17.4-64.2c0.2-0.6,0.6-0.8,1.2-0.8h64.1 c0.6,0,1,0.4,1.2,0.8l17.4,64.2C1496.1,358.4,1496.6,358.8,1497,358.8L1497,358.8z M1420.9,261.7l23.3-85.9c0.4-1.1,1.9-1.1,2.3,0 l23.3,85.9c0.2,0.8-0.4,1.5-1.1,1.5H1422C1421.2,263.2,1420.7,262.4,1420.9,261.7L1420.9,261.7z" />
+          <path fill="currentColor" d="M1991.8,358.8h29c0.8,0,1.3-0.8,1.2-1.5L1962,136.8c-0.2-0.6-0.6-0.8-1.1-0.8h-11.1h-19.5h-11.1 c-0.6,0-1,0.4-1.2,0.8l-59.9,220.5c-0.2,0.8,0.4,1.5,1.1,1.5h29c0.6,0,1-0.4,1.2-0.8l17.4-64.2c0.2-0.6,0.6-0.8,1.1-0.8h64.1 c0.6,0,1,0.4,1.2,0.8l17.4,64.2C1990.8,358.4,1991.2,358.8,1991.8,358.8L1991.8,358.8z M1915.6,261.7l23.3-85.9 c0.4-1.1,1.9-1.1,2.3,0l23.3,85.9c0.2,0.8-0.4,1.5-1.1,1.5h-46.6C1916,263.2,1915.4,262.4,1915.6,261.7L1915.6,261.7z" />
+          <path fill="currentColor" d="M973.4,326.2V258c0-0.6,0.6-1.2,1.2-1.2h84.9c0.6,0,1.1-0.6,1.1-1.1v-27.4c0-0.6-0.6-1.2-1.1-1.2h-84.9 c-0.6,0-1.2-0.6-1.2-1.1v-58.3c0-0.6,0.6-1.1,1.2-1.1h96.6c0.6,0,1.2-0.6,1.2-1.2v-27.4c0-0.6-0.6-1.2-1.2-1.2h-97.7h-33 c-0.6,0-1.1,0.6-1.1,1.2v28.6v60.7v29.7v70.4v30.1c0,0.6,0.6,1.1,1.1,1.1h33h101.7c0.6,0,1.2-0.6,1.2-1.1v-28.9 c0-0.6-0.6-1.1-1.2-1.1H974.4C973.8,327.3,973.4,327,973.4,326.2L973.4,326.2z" />
+          <path fill="currentColor" d="M2499.5,356.9l-110.3-113.7c-0.4-0.4-0.4-1.2,0-1.5l99.3-102.8c0.8-0.8,0.2-1.9-0.8-1.9H2447 c-0.4,0-0.6,0.2-0.8,0.4l-84.2,87.2c-0.8,0.8-1.9,0.2-1.9-0.8v-85.7c0-0.6-0.6-1.2-1.2-1.2h-31.9c-0.6,0-1.1,0.6-1.1,1.2v219.7 c0,0.6,0.6,1.2,1.1,1.2h31.9c0.6,0,1.2-0.6,1.2-1.2v-96.7c0-1,1.3-1.5,1.9-0.8l95.4,98.2c0.2,0.2,0.6,0.4,0.8,0.4h40.7 C2499.7,358.8,2500.3,357.4,2499.5,356.9L2499.5,356.9z" />
+          <g>
+            <polygon fill="#959595" stroke="#959595" strokeWidth="0.78" strokeLinecap="round" strokeLinejoin="round" strokeMiterlimit="22.927" points="470.1,18.5 277.1,161.3 313,77.2" />
+            <polygon fill="#959595" stroke="#959595" strokeWidth="0.78" strokeLinecap="round" strokeLinejoin="round" strokeMiterlimit="22.927" points="25,18.5 216.2,162.7 182,77.2" />
+            <polygon fill="#959595" stroke="#959595" strokeWidth="0.78" strokeLinecap="round" strokeLinejoin="round" strokeMiterlimit="22.927" points="400.6,349.6 349.3,428.1 459.2,458.3 490.7,351.3" />
+            <polygon fill="#959595" stroke="#959595" strokeWidth="0.78" strokeLinecap="round" strokeLinejoin="round" strokeMiterlimit="22.927" points="4.5,351.3 35.8,458.3 145.6,428.1 94.4,349.6" />
+            <polygon fill="#959595" stroke="#E27625" strokeWidth="0.78" strokeLinecap="round" strokeLinejoin="round" strokeMiterlimit="22.927" points="139.7,217.1 109.1,263.2 217.9,268.1 214.3,151" />
+            <polygon fill="#959595" stroke="#E27625" strokeWidth="0.78" strokeLinecap="round" strokeLinejoin="round" strokeMiterlimit="22.927" points="355.4,217.1 279.6,149.7 277.1,268.1 385.9,263.2" />
+            <polygon fill="#959595" stroke="#959595" strokeWidth="0.78" strokeLinecap="round" strokeLinejoin="round" strokeMiterlimit="22.927" points="145.6,428.1 211.4,396.3 154.7,352.1" />
+            <polygon fill="#959595" stroke="#959595" strokeWidth="0.78" strokeLinecap="round" strokeLinejoin="round" strokeMiterlimit="22.927" points="283.6,396.3 349.3,428.1 340.3,352.1" />
+            <polygon fill="#C4C4C4" stroke="#D5BFB2" strokeWidth="0.78" strokeLinecap="round" strokeLinejoin="round" strokeMiterlimit="22.927" points="349.3,428.1 283.6,396.3 288.9,438.9 288.4,457" />
+            <polygon fill="#C4C4C4" stroke="#D5BFB2" strokeWidth="0.78" strokeLinecap="round" strokeLinejoin="round" strokeMiterlimit="22.927" points="145.6,428.1 206.7,457 206.3,438.9 211.4,396.3" />
+            <polygon fill="#233447" stroke="#233447" strokeWidth="0.78" strokeLinecap="round" strokeLinejoin="round" strokeMiterlimit="22.927" points="207.8,323.9 153.2,307.9 191.8,290.2" />
+            <polygon fill="#233447" stroke="#233447" strokeWidth="0.78" strokeLinecap="round" strokeLinejoin="round" strokeMiterlimit="22.927" points="287.2,323.9 303.2,290.2 342,307.9" />
+            <polygon fill="#828282" stroke="#828282" strokeWidth="0.78" strokeLinecap="round" strokeLinejoin="round" strokeMiterlimit="22.927" points="145.6,428.1 155.1,349.6 94.4,351.3" />
+            <polygon fill="#828282" stroke="#828282" strokeWidth="0.78" strokeLinecap="round" strokeLinejoin="round" strokeMiterlimit="22.927" points="339.9,349.6 349.3,428.1 400.6,351.3" />
+            <polygon fill="#828282" stroke="#959595" strokeWidth="0.78" strokeLinecap="round" strokeLinejoin="round" strokeMiterlimit="22.927" points="385.9,263.2 277.1,268.1 287.2,323.9 303.2,290.2 342,307.9" />
+            <polygon fill="#828282" stroke="#959595" strokeWidth="0.78" strokeLinecap="round" strokeLinejoin="round" strokeMiterlimit="22.927" points="153.2,307.9 191.8,290.2 207.8,323.9 217.9,268.1 109.1,263.2" />
+            <polygon fill="#959595" stroke="#959595" strokeWidth="0.78" strokeLinecap="round" strokeLinejoin="round" strokeMiterlimit="22.927" points="109.1,263.2 154.7,352.1 153.2,307.9" />
+            <polygon fill="#959595" stroke="#959595" strokeWidth="0.78" strokeLinecap="round" strokeLinejoin="round" strokeMiterlimit="22.927" points="342,307.9 340.3,352.1 385.9,263.2" />
+            <polygon fill="#959595" stroke="#959595" strokeWidth="0.78" strokeLinecap="round" strokeLinejoin="round" strokeMiterlimit="22.927" points="217.9,268.1 207.8,323.9 220.6,389.8 223.5,303" />
+            <polygon fill="#959595" stroke="#959595" strokeWidth="0.78" strokeLinecap="round" strokeLinejoin="round" strokeMiterlimit="22.927" points="277.1,268.1 271.7,302.8 274.4,389.8 287.2,323.9" />
+            <polygon fill="#A4A4A4" stroke="#A4A4A4" strokeWidth="0.78" strokeLinecap="round" strokeLinejoin="round" strokeMiterlimit="22.927" points="287.2,323.9 274.4,389.8 283.6,396.3 340.3,352.1 342,307.9" />
+            <polygon fill="#A4A4A4" stroke="#A4A4A4" strokeWidth="0.78" strokeLinecap="round" strokeLinejoin="round" strokeMiterlimit="22.927" points="153.2,307.9 154.7,352.1 211.4,396.3 220.6,389.8 207.8,323.9" />
+            <polygon fill="#B0B0B0" stroke="#C0AC9D" strokeWidth="0.78" strokeLinecap="round" strokeLinejoin="round" strokeMiterlimit="22.927" points="288.4,457 288.9,438.9 284,434.7 211.1,434.7 206.3,438.9 206.7,457 145.6,428.1 167,445.6 210.3,475.5 284.5,475.5 328.1,445.6 349.3,428.1" />
+            <polygon fill="#161616" stroke="#161616" strokeWidth="0.78" strokeLinecap="round" strokeLinejoin="round" strokeMiterlimit="22.927" points="283.6,396.3 274.4,389.8 220.6,389.8 211.4,396.3 206.3,438.9 211.1,434.7 284,434.7 288.9,438.9" />
+            <polygon fill="#4D4D4D" stroke="#4D4D4D" strokeWidth="0.78" strokeLinecap="round" strokeLinejoin="round" strokeMiterlimit="22.927" points="478.3,170.7 494.5,91.8 470.1,18.5 283.6,156.6 355.4,217.1 456.7,246.6 479,220.5 469.3,213.5 484.8,199.4 472.9,190.3 488.4,178.5" />
+            <polygon fill="#4D4D4D" stroke="#4D4D4D" strokeWidth="0.78" strokeLinecap="round" strokeLinejoin="round" strokeMiterlimit="22.927" points="0.5,91.8 16.9,170.7 6.4,178.5 22.1,190.3 10.2,199.4 25.7,213.5 16,220.5 38.3,246.6 139.7,217.1 211.4,156.6 25,18.5" />
+            <polygon fill="#A4A4A4" stroke="#A4A4A4" strokeWidth="0.78" strokeLinecap="round" strokeLinejoin="round" strokeMiterlimit="22.927" points="456.7,246.6 355.4,217.1 385.9,263.2 340.3,352.1 400.6,351.3 490.7,351.3" />
+            <polygon fill="#A4A4A4" stroke="#A4A4A4" strokeWidth="0.78" strokeLinecap="round" strokeLinejoin="round" strokeMiterlimit="22.927" points="139.7,217.1 38.3,246.6 4.5,351.3 94.4,351.3 154.7,352.1 109.1,263.2" />
+            <polygon fill="#A4A4A4" stroke="#A4A4A4" strokeWidth="0.78" strokeLinecap="round" strokeLinejoin="round" strokeMiterlimit="22.927" points="277.1,268.1 283.6,156.6 313,77.2 182,77.2 211.4,156.6 217.9,268.1 220.4,303.2 220.6,389.8 274.4,389.8 274.6,303.2" />
+          </g>
+        </g>
+      </svg>
+    </div>
+  );
+};
